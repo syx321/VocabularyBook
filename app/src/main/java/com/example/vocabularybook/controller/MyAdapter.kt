92047331fbf0c983398ea1a7f349a249c974b0ba
syx321 <1580925976@qq.com>
2021-10-06 17:56:04 +0800

    [NewFeature]完成Adapter

--- app/src/main/java/com/example/vocabularybook/controller/MyAdapter.kt
+++ app/src/main/java/com/example/vocabularybook/controller/MyAdapter.kt
@@ -13,6 +13,18 @@ import com.example.vocabularybook.model.listItem
 class MyAdapter(activity: Activity, val resourceId: Int, data: List<listItem>) :
     ArrayAdapter<listItem>(activity, resourceId, data) {
 
+    @SuppressLint("ViewHolder")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = LayoutInflater.from(context).inflate(resourceId, parent, false)
+        val english = view.findViewById<TextView>(R.id.item_content)
+        val time = view.findViewById<TextView>(R.id.item_time)
 
+        val item = getItem(position)
+        if (item != null) {
+            english.text = item.english
+            time.text = item.time
+        }
+        return view
+    }
 
 }
